ea01c66aab2a169f37606e83d640b4a49afc76f1
agmorelli <augusto_morelli05@yahoo.com.ar>
2015-09-24 01:38:11 -0300

    Create libex.h

---
+++ libex.h
@@ -0,0 +1,6 @@
+int menu(char*);
+void alta(int*,char[][51],int*,int*, int, int);
+void modificar(int*,char [][51],int*,int*, int, int);
+void baja(int*,char [][51],int*,int*, int, int);
+void mayor(int*,char [][51],int*,int*, int, int);
+void lista(int*,char [][51],int*,int*, int,int);
